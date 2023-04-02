6ec20b4904331a3cc5b83a2f6fdfc9b563bc0e97
Azat <aelchikushuulu@gmail.com>
2023-04-02 22:25:40 +0600

    /calculator

---
+++ src/main/java/com/github/aza0602/calculator/controller/CalculatorController.java
@@ -0,0 +1,13 @@
+package com.github.aza0602.calculator.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalculatorController {
+
+    @GetMapping("/calculator")
+    public String hello() {
+        return "<h1 style=\"text-align: center\">Добро пожаловать в калькулятор</h1>";
+    }
+}
