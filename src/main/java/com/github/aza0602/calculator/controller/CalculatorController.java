feb51f4e00f7a4566fd6e59597be941808750f64
Azat <aelchikushuulu@gmail.com>
2023-04-02 23:07:55 +0600

    Service

--- src/main/java/com/github/aza0602/calculator/controller/CalculatorController.java
+++ src/main/java/com/github/aza0602/calculator/controller/CalculatorController.java
@@ -1,34 +1,43 @@
 package com.github.aza0602.calculator.controller;
 
+import com.github.aza0602.calculator.service.CalculatorService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/calculator")
 public class CalculatorController {
 
-    @GetMapping("/calculator")
+    private final CalculatorService calculatorService;
+
+    public CalculatorController(CalculatorService calculatorService) {
+        this.calculatorService = calculatorService;
+    }
+
+    @GetMapping("/")
     public String hello() {
         return "<h1 style=\"text-align: center\">Добро пожаловать в калькулятор</h1>";
     }
 
-    @GetMapping("/calculator/plus")
+    @GetMapping("/plus")
     public String plus(@RequestParam("num1") float a, @RequestParam("num2") float b) {
-        return a + " + " + b + " = " + (a + b);
+        return a + " + " + b + " = " + calculatorService.plus(a, b);
     }
 
-    @GetMapping("/calculator/minus")
+    @GetMapping("/minus")
     public String minus(@RequestParam("num1") float a, @RequestParam("num2") float b) {
-        return a + " - " + b + " = " + (a - b);
+        return a + " - " + b + " = " + calculatorService.minus(a, b);
     }
 
-    @GetMapping("/calculator/multiply")
+    @GetMapping("/multiply")
     public String multiply(@RequestParam("num1") float a, @RequestParam("num2") float b) {
-        return a + " * " + b + " = " + (a * b);
+        return a + " * " + b + " = " + calculatorService.multiply(a, b);
     }
 
-    @GetMapping("/calculator/divide")
+    @GetMapping("/divide")
     public String divide(@RequestParam("num1") float a, @RequestParam("num2") float b) {
-        return a + " / " + b + " = " + (a / b);
+        return a + " / " + b + " = " + calculatorService.divide(a, b);
     }
 }
